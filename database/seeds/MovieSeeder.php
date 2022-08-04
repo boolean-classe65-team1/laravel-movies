4a60fc8c7ceefe982026f7da9caafdb5d963d1bb
paolopu91 <paolo.pugliese1991@gmail.com>
2022-08-04 17:38:33 +0200

    cahnged data in date

--- database/seeds/MovieSeeder.php
+++ database/seeds/MovieSeeder.php
@@ -17,70 +17,70 @@ class MovieSeeder extends Seeder
                 "title"=>"Il Padrino " ,
                 "original_title"=>" The Godfather" ,
                 "nationality"=>"american",
-                "data"=> "1972-03-24",
+                "date"=> "1972-03-24",
                 "vote"=>"9.2" ,
             ],
             [
                 "title"=>"Via col vento " ,
                 "original_title"=>" Gone with wind" ,
                 "nationality"=>"american",
-                "data"=> "1939-12-15",
+                "date"=> "1939-12-15",
                 "vote"=>"8.6" ,
             ],
             [
                 "title"=>"Psycho " ,
                 "original_title"=>"Psycho" ,
                 "nationality"=>"american",
-                "data"=> "1960-06-16",
+                "date"=> "1960-06-16",
                 "vote"=>"9.4" ,
             ],
             [
                 "title"=>"Gravity " ,
                 "original_title"=>" Gravity" ,
                 "nationality"=>"american/british",
-                "data"=> "2013-08-28",
+                "date"=> "2013-08-28",
                 "vote"=>"7.8" ,
             ],
             [
                 "title"=>"Toy Story - Il Mondo Dei Giocattoli " ,
                 "original_title"=>" Toy Story" ,
                 "nationality"=>"american/british",
-                "data"=> "1995-11-19",
+                "date"=> "1995-11-19",
                 "vote"=>"9.0" ,
             ],
             [
                 "title"=>"Pulp fiction " ,
                 "original_title"=>" Pulp fiction" ,
                 "nationality"=>"american",
-                "data"=> "1994-10-14",
+                "date"=> "1994-10-14",
                 "vote"=>"9.2" ,
             ],
             [
                 "title"=>"Forrest Gump " ,
                 "original_title"=>" Forrest Gump" ,
                 "nationality"=>"american/british",
-                "data"=> "1994-06-23",
+                "date"=> "1994-06-23",
                 "vote"=>"8.8" ,
             ],
             [
                 "title"=>"Guerre Stellari " ,
                 "original_title"=>" Star Wars" ,
                 "nationality"=>"american",
-                "data"=> "1977-05-25",
+                "date"=> "1977-05-25",
                 "vote"=>"10" ,
             ],
             [
                 "title"=>"E.T. - L'extra terrestre " ,
                 "original_title"=>" E.T. - The Extra Terrestrial" ,
                 "nationality"=>"american",
-                "data"=> "1982-05-26",
+                "date"=> "1982-05-26",
                 "vote"=>"7.9" ,
             ],
             [
                 "title"=>"Il silenzio degli innocenti" ,
                 "original_title"=>"The Silence of the Lambs" ,
                 "nationality"=>"american",
-                "data"=> "1991-01-30",
+                "date"=> "1991-01-30",
                 "vote"=>"8.6" ,
             ]
         ];
@@ -91,7 +91,7 @@ class MovieSeeder extends Seeder
             $data->title = $movie['title'];
             $data->original_title = $movie['original_title'];
             $data->nationality = $movie['nationality'];
-            $data->data = $movie['data'];
+            $data->date = $movie['data'];
             $data->vote = $movie['vote'];
 
             $data->save();
