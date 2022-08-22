557fa3f35c446259f828cf14d08ed2c64f7bae3c
felicetto13 <felice.laterza93@icloud.com>
2022-08-22 16:01:47 +0200

    logout part and edit admin home and guest home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
+
+@section('page_title', 'Home')
+@section('main_content')
+<header>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <ul class="list-unstyled d-flex gap-3 py-3 justify-content-center">
+        <li><a href="{{ route("home") }}">Home</a></li>
+        <li><a href="{{ route("register") }}">Registrati</a></li>
+        <li><a href="{{ route("login") }}">Login</a></li>
+    </ul>
+</div>
+</header>
+<div class="container text-center">
+    <h1>Benvenuti nel nostro sito!</h1>
 </div>
 @endsection
