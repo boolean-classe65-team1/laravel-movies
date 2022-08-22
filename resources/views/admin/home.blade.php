25b62ad65a5b2ec2ea8b0281dff555c7d4dfbf76
NickAstley <andreanicastro1994@gmail.com>
2022-08-22 17:59:02 +0200

    Fiex buttons in admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,8 +7,8 @@
     <div class="d-flex flex-column vh-100">
         <h1 class="py-3">Sei Loggato!</h1>
         <div class="d-flex gap-4 justify-content-center align-items-center flex-grow-1">
-            <div><a class="btn btn-danger" href="{{ route('admin.movies.index') }}">Movies</a></div>
-            <div><a class="btn btn-danger" href="{{ route('admin.tv_series.index') }}">Tv Series</a></div>
+            <div><a class="btn btn-danger fs-1" href="{{ route('admin.movies.index') }}">Movies</a></div>
+            <div><a class="btn btn-danger fs-1" href="{{ route('admin.tv_series.index') }}">Tv Series</a></div>
         </div>
     </div>
 </div>
