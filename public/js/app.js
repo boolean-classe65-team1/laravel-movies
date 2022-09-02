a45666a36473ae6d0de78dc1b4b0588103aa69af
felicetto13 <felice.laterza93@icloud.com>
2022-09-02 17:02:30 +0200

    edit file webpack

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Fonzie\Documents\Boolean\fonzie code\laravel-movies\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Fonzie\Documents\Boolean\fonzie code\laravel-movies\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/felicelaterza/boolean/laravel-movies/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/felicelaterza/boolean/laravel-movies/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
