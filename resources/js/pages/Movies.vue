9c786efcdd9614742778131f14ed452a70dd6d06
fonzhousefonzie <harth.fonz@gmail.com>
2022-09-02 18:10:46 +0200

    fixes spacing in movies cards

--- resources/js/pages/Movies.vue
+++ resources/js/pages/Movies.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row row-cols-3">
             <div class="col" v-for="movie in movies" :key="movie.id">
-                <div class="card h-100">
+                <div class="card h-75 my-3">
                     <div class="card-body">
                         <h5 class="card-title">{{ movie.title }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{ movie.original_title }}</h6>
